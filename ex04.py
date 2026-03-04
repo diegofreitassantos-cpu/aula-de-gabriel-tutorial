82b2c91d5dcea39ba3e1a4e4d291ffbd9249e51b
diegofreitassantos-cpu <diego.freitas.santos@escola.pr.gov.br>
2026-03-04 19:56:30 -0300

    Update ex04.py

--- ex04.py
+++ ex04.py
@@ -1,6 +1,7 @@
 #solicitando dados do usuario
 nome = input("digite seu nome")
-idade = input("digite sua idade")
+age = input("digite sua idade")
 #mostra as informaçoes gravadas no sistema
 print(f"bem vindo {nome}, voce tem {idade} anos")
+
 
